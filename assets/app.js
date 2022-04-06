e688be5456299dab4d19d965da0c53f18025018c
lil_nurik <98051184+Buriev-Nurmuhammad@users.noreply.github.com>
2022-04-06 23:00:13 +0500

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,51 @@
+btn.onclick = () => {
+  let text = document.getElementById("tf");
+  let abuse = [
+    "xuy",
+    "blyat",
+    "beka",
+    "suka",
+    "eban",
+    "dalbayob",
+    "chmo",
+    "oneyni",
+    "pizda",
+    "хуй",
+    "блять",
+    "бека",
+    "сука",
+    "ебан",
+    "далбаёб",
+    "чмо",
+    "онейни",
+    "пизда",
+    "чорт",
+    "Blyat",
+    "Beka",
+    "Suka",
+    "Eban",
+    "Dalbayob",
+    "Chmo",
+    "Oneyni",
+    "Pizda",
+    "Хуй",
+    "Блять",
+    "Бека",
+    "Сука",
+    "Ебан",
+    "Далбаёб",
+    "Чмо",
+    "Онейни",
+    "Пизда",
+    "Чорт",
+  ];
+  let btn = document.getElementById("btn");
+  let tv = text.value;
+
+  console.log(
+    tv
+      .split(" ")
+      .map((a) => (abuse.some((elem) => elem == a) ? "*".repeat(a.length) : a))
+      .join(" ")
+  );
+};
